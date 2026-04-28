1a007bf0dc68360e296d93d66844ea92eddbb2fe
lobatoallan8@gmail.com <lobatoallan8@gmail.com>
2026-04-28 20:12:00 -0300

    Coloquei o dataGrid

--- WindowsFormsPaoDoce/Views/RelatorioEstoqueForm.Designer.cs
+++ WindowsFormsPaoDoce/Views/RelatorioEstoqueForm.Designer.cs
@@ -17,22 +17,42 @@ namespace WindowsFormsPaoDoce.Views
 
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(153, 146);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(864, 384);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // RelatorioEstoqueForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1004, 601);
+            this.ClientSize = new System.Drawing.Size(1339, 740);
+            this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "RelatorioEstoqueForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Relatorio de Estoque Atual — Allan";
+            this.Load += new System.EventHandler(this.RelatorioEstoqueForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- WindowsFormsPaoDoce/Views/RelatorioEstoqueForm.cs
+++ WindowsFormsPaoDoce/Views/RelatorioEstoqueForm.cs
@@ -1,4 +1,8 @@
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 using System.Windows.Forms;
+
 
 namespace WindowsFormsPaoDoce.Views
 {
@@ -7,6 +11,40 @@ namespace WindowsFormsPaoDoce.Views
         public RelatorioEstoqueForm()
         {
             InitializeComponent();
+        }
+        private void CarregarEstoque()
+        {
+            string conexao = "Server=62.171.167.217;Port=33060;Database=db_padaria;Uid=root_padaria;Pwd=Pad2026SecPad;";
+
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+
+                    string query = "SELECT * FROM vw_estoque_atual";
+
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                    DataTable tabela = new DataTable();
+
+                    adapter.Fill(tabela);
+
+                    dataGridView1.DataSource = tabela;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.Message);
+                }
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void RelatorioEstoqueForm_Load(object sender, EventArgs e)
+        {
+            CarregarEstoque();
         }
     }
 }
