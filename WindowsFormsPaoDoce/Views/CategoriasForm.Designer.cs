d081087d117cfc6a048c01c1ad2268b76f2c0b9d
artur <arturteixeirafurlannova@gmail.com>
2026-04-28 21:36:37 -0300

    teste de botoes, INCOMPLETO!

--- WindowsFormsPaoDoce/Views/CategoriasForm.Designer.cs
+++ WindowsFormsPaoDoce/Views/CategoriasForm.Designer.cs
@@ -17,22 +17,293 @@ namespace WindowsFormsPaoDoce.Views
 
         private void InitializeComponent()
         {
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.gridDados = new System.Windows.Forms.DataGridView();
+            this.grpCadastro = new System.Windows.Forms.GroupBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.chkAtivo = new System.Windows.Forms.CheckBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.panelCabecalho = new System.Windows.Forms.Panel();
+            this.lblSubtitulo = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDados)).BeginInit();
+            this.grpCadastro.SuspendLayout();
+            this.panelCabecalho.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.BackColor = System.Drawing.Color.White;
+            this.btnVoltar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltar.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            this.btnVoltar.Location = new System.Drawing.Point(931, 640);
+            this.btnVoltar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(187, 44);
+            this.btnVoltar.TabIndex = 7;
+            this.btnVoltar.Text = "Voltar ao Menu";
+            this.btnVoltar.UseVisualStyleBackColor = false;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // gridDados
+            // 
+            this.gridDados.AllowUserToAddRows = false;
+            this.gridDados.AllowUserToDeleteRows = false;
+            this.gridDados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridDados.BackgroundColor = System.Drawing.Color.White;
+            this.gridDados.ColumnHeadersHeight = 36;
+            this.gridDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.gridDados.EnableHeadersVisualStyles = false;
+            this.gridDados.Location = new System.Drawing.Point(36, 279);
+            this.gridDados.Margin = new System.Windows.Forms.Padding(4);
+            this.gridDados.MultiSelect = false;
+            this.gridDados.Name = "gridDados";
+            this.gridDados.ReadOnly = true;
+            this.gridDados.RowHeadersVisible = false;
+            this.gridDados.RowHeadersWidth = 51;
+            this.gridDados.RowTemplate.Height = 30;
+            this.gridDados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridDados.Size = new System.Drawing.Size(1092, 323);
+            this.gridDados.TabIndex = 6;
+            this.gridDados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridDados_CellContentClick);
+            // 
+            // grpCadastro
+            // 
+            this.grpCadastro.Controls.Add(this.txtId);
+            this.grpCadastro.Controls.Add(this.btnLimpar);
+            this.grpCadastro.Controls.Add(this.btnExcluir);
+            this.grpCadastro.Controls.Add(this.btnEditar);
+            this.grpCadastro.Controls.Add(this.btnSalvar);
+            this.grpCadastro.Controls.Add(this.chkAtivo);
+            this.grpCadastro.Controls.Add(this.txtDescricao);
+            this.grpCadastro.Controls.Add(this.lblDescricao);
+            this.grpCadastro.Controls.Add(this.txtNome);
+            this.grpCadastro.Controls.Add(this.lblNome);
+            this.grpCadastro.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.grpCadastro.Location = new System.Drawing.Point(27, 113);
+            this.grpCadastro.Margin = new System.Windows.Forms.Padding(4);
+            this.grpCadastro.Name = "grpCadastro";
+            this.grpCadastro.Padding = new System.Windows.Forms.Padding(4);
+            this.grpCadastro.Size = new System.Drawing.Size(1101, 148);
+            this.grpCadastro.TabIndex = 5;
+            this.grpCadastro.TabStop = false;
+            this.grpCadastro.Text = "Dados";
+            this.grpCadastro.Enter += new System.EventHandler(this.grpCadastro_Enter);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.White;
+            this.btnLimpar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            this.btnLimpar.Location = new System.Drawing.Point(707, 84);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(133, 44);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnExcluir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExcluir.Enabled = false;
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            this.btnExcluir.ForeColor = System.Drawing.Color.White;
+            this.btnExcluir.Location = new System.Drawing.Point(560, 84);
+            this.btnExcluir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(133, 44);
+            this.btnExcluir.TabIndex = 7;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btnEditar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEditar.Enabled = false;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            this.btnEditar.Location = new System.Drawing.Point(413, 84);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(133, 44);
+            this.btnEditar.TabIndex = 6;
+            this.btnEditar.Text = "Atualizar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(181)))), ((int)(((byte)(95)))));
+            this.btnSalvar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            this.btnSalvar.Location = new System.Drawing.Point(267, 84);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(133, 44);
+            this.btnSalvar.TabIndex = 5;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // chkAtivo
+            // 
+            this.chkAtivo.AutoSize = true;
+            this.chkAtivo.Checked = true;
+            this.chkAtivo.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAtivo.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.chkAtivo.Location = new System.Drawing.Point(20, 84);
+            this.chkAtivo.Margin = new System.Windows.Forms.Padding(4);
+            this.chkAtivo.Name = "chkAtivo";
+            this.chkAtivo.Size = new System.Drawing.Size(71, 27);
+            this.chkAtivo.TabIndex = 4;
+            this.chkAtivo.Text = "Ativo";
+            this.chkAtivo.UseVisualStyleBackColor = true;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtDescricao.Location = new System.Drawing.Point(613, 36);
+            this.txtDescricao.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(319, 30);
+            this.txtDescricao.TabIndex = 3;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblDescricao.Location = new System.Drawing.Point(500, 39);
+            this.lblDescricao.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(87, 23);
+            this.lblDescricao.TabIndex = 2;
+            this.lblDescricao.Text = "Descricao:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtNome.Location = new System.Drawing.Point(100, 36);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(372, 30);
+            this.txtNome.TabIndex = 1;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblNome.Location = new System.Drawing.Point(20, 39);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(61, 23);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome:";
+            // 
+            // panelCabecalho
+            // 
+            this.panelCabecalho.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(181)))), ((int)(((byte)(95)))));
+            this.panelCabecalho.Controls.Add(this.lblSubtitulo);
+            this.panelCabecalho.Controls.Add(this.lblTitulo);
+            this.panelCabecalho.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCabecalho.Location = new System.Drawing.Point(0, 0);
+            this.panelCabecalho.Margin = new System.Windows.Forms.Padding(4);
+            this.panelCabecalho.Name = "panelCabecalho";
+            this.panelCabecalho.Size = new System.Drawing.Size(1160, 94);
+            this.panelCabecalho.TabIndex = 4;
+            // 
+            // lblSubtitulo
+            // 
+            this.lblSubtitulo.AutoSize = true;
+            this.lblSubtitulo.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lblSubtitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.lblSubtitulo.Location = new System.Drawing.Point(32, 59);
+            this.lblSubtitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSubtitulo.Name = "lblSubtitulo";
+            this.lblSubtitulo.Size = new System.Drawing.Size(338, 21);
+            this.lblSubtitulo.TabIndex = 1;
+            this.lblSubtitulo.Text = "Referencia para o grupo — pode deletar depois";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.ForeColor = System.Drawing.Color.Black;
+            this.lblTitulo.Location = new System.Drawing.Point(27, 14);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(249, 37);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Exemplo de CRUD";
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtId.Location = new System.Drawing.Point(99, 84);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(121, 30);
+            this.txtId.TabIndex = 9;
+            this.txtId.TextChanged += new System.EventHandler(this.txtId_TextChanged);
             // 
             // CategoriasForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(764, 521);
+            this.ClientSize = new System.Drawing.Size(1160, 708);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.gridDados);
+            this.Controls.Add(this.grpCadastro);
+            this.Controls.Add(this.panelCabecalho);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "CategoriasForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de Categorias — Artur";
+            this.Load += new System.EventHandler(this.CategoriasForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridDados)).EndInit();
+            this.grpCadastro.ResumeLayout(false);
+            this.grpCadastro.PerformLayout();
+            this.panelCabecalho.ResumeLayout(false);
+            this.panelCabecalho.PerformLayout();
             this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.DataGridView gridDados;
+        private System.Windows.Forms.GroupBox grpCadastro;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.CheckBox chkAtivo;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Panel panelCabecalho;
+        private System.Windows.Forms.Label lblSubtitulo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtId;
     }
 }
--- WindowsFormsPaoDoce/Views/CategoriasForm.cs
+++ WindowsFormsPaoDoce/Views/CategoriasForm.cs
@@ -1,12 +1,155 @@
+using MySql.Data.MySqlClient;
+using System;
 using System.Windows.Forms;
 
 namespace WindowsFormsPaoDoce.Views
 {
     public partial class CategoriasForm : Form
     {
+        string conexao = "Server=62.171.167.217;Port=33060;Database=db_padaria;Uid=root_padaria;Pwd=Pad2026SecPad;";
         public CategoriasForm()
         {
             InitializeComponent();
+        }
+
+        private void TestarConexao()
+        {
+
+
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.Message);
+                }
+            }
+        }
+        private void CategoriasForm_Load(object sender, System.EventArgs e)
+        {
+            txtId.Visible = false;
+            gridDados();
+        }
+
+        private void btnSalvar_Click(object sender, System.EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+            {
+                try
+                {
+                    conn.Open();
+
+                    string sql = @"INSERT INTO categorias 
+                           (nome, descricao, ativo) 
+                           VALUES (@nome, @descricao, @ativo)";
+
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = txtNome.Text;
+                        cmd.Parameters.Add("@descricao", MySqlDbType.VarChar).Value = txtDescricao.Text;
+                        cmd.Parameters.Add("@ativo", MySqlDbType.Bit).Value = chkAtivo.Checked;
+
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show("Salvo com sucesso!");
+                    btnLimpar.Show();
+                    CarregarGrid(); // opcional
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexao))
+                {
+                conn.Open();
+
+                string sql = @"UPTADE categorias SET nome=@nome, descriçao=@descriçao, ativo=@ativo WHERE id=@id";
+
+            using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                    cmd.Parameters.AddWithValue("@descriçao", txtDescricao.Text);
+                    cmd.Parameters.AddWithValue("@ativo", chkAtivo.Text);
+                    cmd.Parameters.AddWithValue("@id", txtId);
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Atualizado!");
+                gridDados();
+        }
+    }
+
+        private void txtId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gridDados_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            using (MySqlConnection coon = new MySqlConnection(conexao))
+            {
+                coon.Open();
+
+                string sql = "SELECT * FROM categorias";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, coon);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                DataGridView.DataSource = dt;
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja excluir?", "Confirmção", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (MySqlConnection coon = new MySqlConnection(conexao))
+                {
+                    coon.Open();
+
+                    string sql = "DELETE FROM categorias WHERE id=@id";
+
+                    using (MySqlCommand cmd = new MySqlCommand(sql, coon))
+                    {
+                        cmd.Parameters.AddWithValue("@id", txtId);
+                        cmd.ExecuteNonQuery ();
+                    }
+                }
+
+                MessageBox.Show("Excluído!");
+                gridDados();
+            }
+
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Clear();
+            txtDescricao.Clear();
+            chkAtivo.Checked = false;
+        }
+
+        private void grpCadastro_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
